55f5e5ef59feaf1a4861bedcbd60862430e19041
mason-eisch <82910799+mason-eisch@users.noreply.github.com>
2023-02-28 17:01:18 -0600

    Removes irrelevant test

--- client/cypress/e2e/app.cy.ts
+++ client/cypress/e2e/app.cy.ts
@@ -5,10 +5,6 @@ const page = new AppPage();
 describe('App', () => {
   beforeEach(() => page.navigateTo());
 
-  it('Should have the correct title', () => {
-    page.getAppTitle().should('contain', 'CSci 3601 Lab 4');
-  });
-
   it('The sidenav should open, navigate to "Users" and back to "Home"', () => {
     // Before clicking on the button, the sidenav should be hidden
     page.getSidenav()
